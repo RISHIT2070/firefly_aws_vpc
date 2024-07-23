6c8233897fa129d5bf11881390374a6054005c98
RISHIT2070 <95566422+RISHIT2070@users.noreply.github.com>
2024-07-23 20:26:26 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,49 @@
+provider "aws" {
+  region = "ap-south-1"
+
+  default_tags {
+    tags = {
+      "Environment" = var.environment
+      "createdby"   = "Infrasity"
+    }
+  }
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+data "aws_availability_zones" "available" {}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.8.1"
+
+  name                    = "Infrasity-vpc"
+  cidr                    = "10.42.0.0/16"
+  azs                     = slice(data.aws_availability_zones.available.names, 0, 2)
+  public_subnets          = ["10.42.0.0/24", "10.42.1.0/24"]
+  map_public_ip_on_launch = true
+}
+
+resource "aws_instance" "web" {
+  ami                    = data.aws_ami.ubuntu.image_id
+  instance_type          = "t3.micro"
+  subnet_id              = module.vpc.public_subnets[0]
+  vpc_security_group_ids = [module.vpc.default_security_group_id]
+
+  tags = {
+    "Environment" = var.environment
+    "createdby"   = "Infrasity"
+  }
+}
